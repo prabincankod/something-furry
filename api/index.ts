dd6e204c7017d92ca11e4329dcd287244eb5e1ef
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2022-03-27 13:56:34 +0545

    Set redirect to techcube's github

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  res.status(200).send("<h1>hemlo world</h1>");
+  res.redirect("https://discord.gg/gXcrfxSurk");
 };
